7993602ed9ee0f934ede346835753807048e87d1
David Valderrama <dvalderrama08@gmail.com>
2021-04-10 23:44:06 -0700

    Added initial row styling

--- scheduler.js
+++ scheduler.js
@@ -12,23 +12,38 @@ $(function() {
         let timeRowTH = $("<th>");
         let timeRowTDContent = $("<td>");
         let timeRowTDSaveButton = $("<td>");
-        timeRowTH.attr("scope", "row")
-        //console.log(t);
+        timeRowTH.attr("scope", "row");
+        timeRowTH.addClass("time-block");
+        
         let hour = t;
         if (hour > 12) {
             hour = hour - 12;
-            console.log(hour);
-            timeRowTH.text(hour + " PM");
+            timeRowTH.text(`${hour} PM`);
+            timeRowTH.addClass("indent");
         } else {
-            console.log(hour);
-            timeRowTH.text(hour + " AM");
+            if (hour < 10) {
+                timeRowTH.text(`${hour} AM`);
+                timeRowTH.addClass("indent");
+            } else {
+                timeRowTH.text(`${hour} AM`);
+            }
         }
 
-        timeRowTDContent.text("content placeholder");
-        timeRowTDSaveButton.text("button placeholder");
+        // Add input element to the cell
+        let inputElem = $("<textarea>");
+        timeRowTDContent.append(inputElem);
+        timeRowTDContent.addClass("past"); // TODO: make this dynamic
+
+        // TODO Add Save Icon Image to the cell
+        let saveIconElem = $("<img>")
+        timeRowTDSaveButton.append(saveIconElem);
+        timeRowTDSaveButton.addClass("saveBtn");
+
+        // Add the content to the row and table body
         let timeRowTR = $("<tr>").append(timeRowTH);
         timeRowTR.append(timeRowTDContent);
         timeRowTR.append(timeRowTDSaveButton);
+        timeRowTR.addClass("row");
         $("tbody").append(timeRowTR);
     }
 
